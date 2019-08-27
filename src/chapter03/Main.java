b641a51044457153023b076eede8600e8a3bbbf9
sangminLee <nalsm98@naver.com>
2019-08-27 22:00:43 +0900

    람다 표현식 세미나 후 업데이트

---
+++ src/chapter03/Main.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.ToIntBiFunction;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Predicate<Integer> predicate = (integer) -> false;
+  }
+
+}
