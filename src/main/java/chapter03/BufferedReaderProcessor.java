595dc590a2ff217b184ef6725d6ad360f1404083
sangminLee <nalsm98@naver.com>
2019-10-01 00:29:00 +0900

    람다 표현식 공부(100%)

---
+++ src/main/java/chapter03/BufferedReaderProcessor.java
@@ -0,0 +1,11 @@
+package chapter03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface BufferedReaderProcessor {
+
+  String process(BufferedReader b) throws IOException;
+
+}
---
+++ src/main/java/chapter03/Main.java
@@ -0,0 +1,11 @@
+package chapter03;
+
+import java.util.function.Predicate;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Predicate<Integer> predicate = (integer) -> false;
+  }
+
+}
